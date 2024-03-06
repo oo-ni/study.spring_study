958372989db800bd05ea19a04b8972d301e3474a
GeonWoo <gdbsrjsdn@gmail.com>
2024-03-07 00:43:01 +0900

    add MemberService & MemberService test

---
+++ inflearn-spring-study/demo/src/main/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // null 반환 시 클라이언트에서 처리를 위해 Optional의 ofNullable 활용.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();     // 뭐라도 찾아지면 바로 반환시킴.
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
