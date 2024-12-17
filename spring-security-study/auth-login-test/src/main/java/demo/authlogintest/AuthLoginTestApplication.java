1f893a6f7797e7039df969c15a5e26289f993f2f
ooni <gdbsrjsdn@gmail.com>
2024-12-17 17:24:12 +0900

    auth-login-test project setting

---
+++ spring-security-study/auth-login-test/src/main/java/demo/authlogintest/AuthLoginTestApplication.java
@@ -0,0 +1,13 @@
+package demo.authlogintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLoginTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthLoginTestApplication.class, args);
+    }
+
+}
