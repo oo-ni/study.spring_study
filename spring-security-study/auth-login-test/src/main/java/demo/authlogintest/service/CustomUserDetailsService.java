7d456fd8d96dc53b145e632c48eedd91d3db2389
ooni <gdbsrjsdn@gmail.com>
2024-12-17 17:29:22 +0900

    email로 유저정보 찾도록 UserRepository, CustomUser 설정.

---
+++ spring-security-study/auth-login-test/src/main/java/demo/authlogintest/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package demo.authlogintest.service;
+
+import demo.authlogintest.entity.User;
+import demo.authlogintest.repository.UserRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다. " + email));
+        return new CustomUserDetails(user);
+    }
+}
